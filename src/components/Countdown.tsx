92d44370464f9b952aef5a6dc7a03037e58d4337
Lucas <lucasdantasmoura@hotmail.com>
2021-02-24 18:29:34 -0300

    Regras de negócio e contextos no react

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,58 +1,83 @@
-import { useState, useEffect } from 'react'
-import styles from '../styles/components/Countdown.module.css'
+import { useState, useEffect, useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContexts";
+import styles from "../styles/components/Countdown.module.css";
+
+let countdownTimeout: NodeJS.Timeout;
 
 export function Countdown() {
-    const [time, setTime] = useState(25 * 60)
-    const [active, setActive] = useState(false)
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
+  const {startNewChallenge} = useContext(ChallengesContext)
+  const [time, setTime] = useState(25 * 60);
+  const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
 
-    //padStart se a string n tiver 2 caracteres ele vai preencher o restante a esquerda com 0
-    const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('')
-    const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('')
+  //padStart se a string n tiver 2 caracteres ele vai preencher o restante a esquerda com 0
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, "0").split("");
+  const [secondLeft, secondRight] = String(seconds).padStart(2, "0").split("");
 
-    function startCountdown() {
-        setActive(true);
+  function startCountdown() {
+    setIsActive(true);
+  }
+
+  function resetCountdown() {
+    clearTimeout(countdownTimeout);
+    setIsActive(false);
+    setTime(25 * 60);
+  }
+
+  useEffect(() => {
+    if (isActive && time > 0) {
+      countdownTimeout = setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    } else if (isActive && time === 0) {
+      setHasFinished(true);
+      setIsActive(false);
+      startNewChallenge()
     }
+  }, [isActive, time]);
 
-    useEffect(() => {
-        if(active && time > 0) {
-            setTimeout(() => {
-                setTime(time - 1)
-            }, 1000)
-        } 
-    }, [active, time])
-
-    return (
+  return (
+    <div>
+      <div className={styles.countdownContainer}>
         <div>
-            <div className={styles.countdownContainer}>
-                <div>
-                    <span>
-                        {minuteLeft}
-                    </span>
-                    <span>
-                        {minuteRight}
-                    </span>
-                </div>
-                <span>:</span>
-                <div>
-                    <span>
-                       {secondLeft}
-                    </span>
-                    <span>
-                       {secondRight}
-                    </span>
-                </div>
-            </div>
-            <div>
-                <button
-                 type="button" 
-                 className={styles.countdownButton}
-                 onClick={startCountdown}
-                 >
-                    Iniciar um ciclo
-                </button>
-            </div>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
         </div>
-    )
+        <span>:</span>
+        <div>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
+        </div>
+      </div>
+      <div>
+        {hasFinished ? (
+          <button disabled className={styles.countdownButton}>
+            Ciclo encerrado
+          </button>
+        ) : (
+          <>
+            {isActive ? (
+              <button
+                type="button"
+                className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+                onClick={resetCountdown}
+              >
+                Abandonar ciclo
+              </button>
+            ) : (
+              <button
+                type="button"
+                className={styles.countdownButton}
+                onClick={startCountdown}
+              >
+                Iniciar um ciclo
+              </button>
+            )}
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 //algo que não muda durante toda a aplicação e que vai repetir em todas as paginas, mas é recalculado
 import '../styles/global.css'
+import { ChallengesProvider } from '../contexts/ChallengesContexts'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <ChallengesProvider>
+    <Component {...pageProps} />
+  </ChallengesProvider>
+  )
 }
 
 export default MyApp
